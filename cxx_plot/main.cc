70596a0b3d291defe0ab8fd7123ece6eedc15a18
XxChang <xuxchang@hotmail.com>
2023-12-05 18:40:02 +0800

    add cxx_plot

--- cxx_plot/main.cc
+++ cxx_plot/main.cc
@@ -1,14 +1,11 @@
-#include <dora-node-api.h>
+#include "rust_part.h"
 #include <iostream>
-#include <arrow/array.h>
-#include <arrow/api.h>
-#include <arrow/io/memory.h>
-#include <arrow/ipc/reader.h>
-#include <arrow/ipc/api.h>
-#include <arrow/util/logging.h>
+#include <opencv4/opencv2/opencv.hpp>
 
 int main() {
     auto dora_node = init_dora_node();
+    cv::namedWindow("image", cv::WINDOW_AUTOSIZE);
+    cv::Mat image;
 
     for (;;) {
         auto event = next_event(dora_node.events);
@@ -18,41 +15,34 @@ int main() {
             break;
         }
         else if (ty == DoraEventType::Input) {
-            auto input = event_as_input(std::move(event));
-            if (std::string(input.id) == "stereo_image") {
-                auto data_ptr = input.data.data();
-                auto size = input.data.size();
-                std::shared_ptr<arrow::Buffer> buffer = arrow::Buffer::Wrap(data_ptr, size);
+            auto input = get_pic_from_event(std::move(event));
 
-                std::vector<std::shared_ptr<arrow::Field>> fields;
-                std::shared_ptr<arrow::Field> width =  arrow::field("width", arrow::uint32());
-                std::shared_ptr<arrow::Field> height = arrow::field("height", arrow::uint32());
-                std::shared_ptr<arrow::Field> raw_data = arrow::field("raw", arrow::fixed_size_list(arrow::uint8(), 453620));
-                fields.push_back(width);
-                fields.push_back(height);
-                fields.push_back(raw_data);
-                std::shared_ptr<arrow::DataType> struct_type = arrow::struct_(fields);
-                std::shared_ptr<arrow::ArrayData> array_data = arrow::ArrayData::Make(
-                    struct_type,
-                    2,
-                    {buffer, buffer, buffer}
-                );
+            cv::Mat l_image{
+                static_cast<int>(input.height_l), 
+                static_cast<int>(input.width_l),
+                CV_8UC1,
+                input.raw_data_l.data()
+            };
 
-                arrow::StructArray struct_array{array_data};
-                arrow::UInt32Array width_array{struct_array.GetFieldByName("width")->data()};
-                // arrow::UInt32Array width_array{struct_array.field(0)->data()};
+            cv::Mat r_image{
+                static_cast<int>(input.height_r), 
+                static_cast<int>(input.width_r),
+                CV_8UC1,
+                input.raw_data_r.data()
+            };
 
-            } else {
-                std::cerr << "Unexpected input " << std::string(input.id) << std::endl;
+
+            cv::vconcat(r_image.clone(), l_image, image);
+            cv::imshow("image", image);
+            if (cv::waitKey(1) > 0) {
                 break;
             }
-        }
-        else
-        {
+        } else {
             std::cerr << "Unknown event type " << static_cast<int>(ty) << std::endl;
             break;
         }
-    }
+    } 
 
+    cv::destroyAllWindows();
     return 0;
 }
---
+++ cxx_plot/rust_part/src/lib.rs
@@ -0,0 +1,122 @@
+use dora_node_api::{
+    Event, arrow::{array::{StructArray, PrimitiveArray, FixedSizeListArray}, datatypes::UInt32Type},
+    EventStream
+};
+use dora_node_api::arrow::datatypes::UInt8Type;
+use eyre::bail;
+
+#[cxx::bridge]
+#[allow(clippy::needless_lifetimes)]
+mod ffi {
+    struct StereoImageInfo {
+        width_r: u32,
+        height_r: u32,
+        width_l: u32,
+        height_l: u32,
+        raw_data_l: Vec<u8>,
+        raw_data_r: Vec<u8>,
+    } 
+
+    struct DoraNode {
+        events: Box<Events>,
+        send_output: Box<OutputSender>,
+    }
+
+    pub enum DoraEventType {
+        Stop,
+        Input,
+        InputClosed,
+        Error,
+        Unknown,
+        AllInputsClosed,
+    }
+
+    struct DoraResult {
+        error: String,
+    }
+
+    extern "Rust" {
+        type Events;
+        type OutputSender;
+        type DoraEvent;
+
+        fn init_dora_node() -> Result<DoraNode>;
+
+        fn next_event(inputs: &mut Box<Events>) -> Box<DoraEvent>;
+        fn event_type(event: &Box<DoraEvent>) -> DoraEventType;
+
+        fn get_pic_from_event(input: Box<DoraEvent>) -> Result<StereoImageInfo>;
+    }
+}
+
+fn init_dora_node() -> eyre::Result<ffi::DoraNode> {
+    let (node, events) = dora_node_api::DoraNode::init_from_env()?;
+    let events = Events(events);
+    let send_output = OutputSender(node);
+
+    Ok(ffi::DoraNode {
+        events: Box::new(events),
+        send_output: Box::new(send_output),
+    })
+}
+
+pub struct Events(EventStream);
+
+fn next_event(events: &mut Box<Events>) -> Box<DoraEvent> {
+    Box::new(DoraEvent(events.0.recv()))
+}
+
+pub struct DoraEvent(Option<Event>);
+
+fn event_type(event: &DoraEvent) -> ffi::DoraEventType {
+    match &event.0 {
+        Some(event) => match event {
+            Event::Stop => ffi::DoraEventType::Stop,
+            Event::Input { .. } => ffi::DoraEventType::Input,
+            Event::InputClosed { .. } => ffi::DoraEventType::InputClosed,
+            Event::Error(_) => ffi::DoraEventType::Error,
+            _ => ffi::DoraEventType::Unknown,
+        },
+        None => ffi::DoraEventType::AllInputsClosed,
+    }
+}
+
+pub struct OutputSender(dora_node_api::DoraNode);
+
+fn get_pic_from_event(input: Box<DoraEvent>) -> eyre::Result<ffi::StereoImageInfo> {
+    let Some(Event::Input { id, metadata: _, data }) = input.0 else {
+        bail!("not an input event");
+    };
+
+    if id.as_str() != "stereo_image" {
+        bail!("not an stereo_image message");
+    }
+
+    let data_inner: StructArray = data.to_data().into();
+    let width: PrimitiveArray<UInt32Type> = data_inner.column_by_name("width").unwrap().to_data().into();
+    let height: PrimitiveArray<UInt32Type> = data_inner.column_by_name("height").unwrap().to_data().into();
+
+    let width_r: u32 = width.value(0).into();
+    let width_l: u32 = width.value(1).into();
+
+    let height_r: u32 = height.value(0).into();
+    let height_l: u32 = height.value(1).into();
+
+    let raw_data_list: FixedSizeListArray = data_inner.column_by_name("raw").unwrap().to_data().into();
+    let raw_data_r: PrimitiveArray<UInt8Type> = raw_data_list.value(0).to_data().into();
+    let raw_data_l: PrimitiveArray<UInt8Type> = raw_data_list.value(1).to_data().into();
+
+    let raw_data_r = raw_data_r.values().to_vec();
+    let raw_data_l = raw_data_l.values().to_vec();
+
+    Ok(ffi::StereoImageInfo {
+        width_r,
+        height_r,
+        width_l,
+        height_l,
+        raw_data_l: raw_data_r,
+        raw_data_r: raw_data_l,
+    })
+}
+
+
